bba1e473931d6a148979407a20e40c91ee5f7fe9
Nick Zirnoon <nickz@windowmark-lm.corp.yahoo.com>
2014-06-05 18:14:43 -0700

    Added a Readme doc

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Nick Zirnoon on 6/4/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
